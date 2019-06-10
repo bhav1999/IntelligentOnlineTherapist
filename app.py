280397552493bf45d1cea1fe636e0fb2c4701123
Deekshitha.P <deekshitha.p@hashedin.com>
2019-06-11 02:02:06 +0530

    Combined the three modules to display appropriate templates and print scores on terminal

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, redirect, url_for , render_template
 import json
 from flask import Response
 import depression_detection_tweets
-
-
-
+import face_recognition
 
 app = Flask(__name__)
 
@@ -12,20 +10,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sentimental_analysis', methods = ['GET'])
+def sentimental_analysis():
+    return render_template('senti-index.html')
 
-@app.route('/sentiment_analysis', methods = ['GET'])
-def sentiment_analysis():
+@app.route('/sentiment', methods = ['GET'])
+def sentiment():
     val= depression_detection_tweets.func("I am happy")
     print(val)
-    data = {
-        "score":val
-    }
-    js = json.dumps(data)
-
-    resp = Response(js, status=200, mimetype='application/json')
+    return render_template("index.html")
 
 
-    return resp
+@app.route('/emotion_detection', methods = ['GET'])
+def emotion_detection():
+    try:
+        val=face_recognition.func()
+        print(val)
+    except:
+        pass
+    return render_template("index.html")
+
+
+@app.route('/BDI', methods = ['GET'])
+def bdi():
+    return render_template('BDI.html')
 
 #@app.route('/employee',methods = ['GET'])
 #def employee()
